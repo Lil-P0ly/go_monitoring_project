856bf3271ff4b6e5e89387d7ea2a19d6abf236f1
Крыжановский Максим <makskryzh@gmail.com>
2026-07-16 23:03:46 +0500

    rebuild app

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,41 +3,26 @@ package main
 import (
 	"fmt"
 	"log"
-	"net/http"
 
+	"github.com/Lil-P0ly/go_monitoring_project/internal/server"
 	"github.com/Lil-P0ly/go_monitoring_project/internal/server/config"
 	"github.com/Lil-P0ly/go_monitoring_project/internal/server/handler"
 	models "github.com/Lil-P0ly/go_monitoring_project/internal/server/model"
-	"github.com/go-chi/chi/v5"
 )
 
 func main() {
-
 	cfg, err := config.ParseFlags()
-
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 	log.Printf("%s", cfg.Address)
 
-	r := chi.NewRouter()
+	storage := models.NewMemStorage()
+	h := handler.NewMSHandlerWithStorage(storage)
 
-	memStorage := models.NewMemStorage()
-
-	msh := handler.NewMSHandlerWithStorage(memStorage)
-
-	r.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		w.Write([]byte("Method Not Allowed"))
-	})
-
-	r.Get("/", msh.PrintMetricsHTML)
-	r.Post("/update/{metrics_type}/{metrics_name}/{metrics_value}", msh.AddValue)
-	r.Get("/value/{metrics_type}/{metrics_name}", msh.PrintLastValue)
-
-	if err := http.ListenAndServe(cfg.Address, r); err != nil {
+	srv := server.New(cfg, h)
+	if err := srv.Run(); err != nil {
 		log.Fatal(err)
 	}
-
 }
---
+++ internal/server/server.go
@@ -0,0 +1,39 @@
+package server
+
+import (
+	"net/http"
+
+	"github.com/Lil-P0ly/go_monitoring_project/internal/server/config"
+	"github.com/Lil-P0ly/go_monitoring_project/internal/server/handler"
+	"github.com/go-chi/chi/v5"
+)
+
+type Server struct {
+	cfg     config.Config
+	handler *handler.MemoryStorageHandler
+	router  *chi.Mux
+}
+
+func New(cfg config.Config, h *handler.MemoryStorageHandler) *Server {
+	s := &Server{
+		cfg:     cfg,
+		handler: h,
+		router:  chi.NewRouter(),
+	}
+	s.routes()
+	return s
+}
+
+func (s *Server) routes() {
+	s.router.MethodNotAllowed(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("Method Not Allowed"))
+	})
+	s.router.Get("/", s.handler.PrintMetricsHTML)
+	s.router.Post("/update/{metrics_type}/{metrics_name}/{metrics_value}", s.handler.AddValue)
+	s.router.Get("/value/{metrics_type}/{metrics_name}", s.handler.PrintLastValue)
+}
+
+func (s *Server) Run() error {
+	return http.ListenAndServe(s.cfg.Address, s.router)
+}
